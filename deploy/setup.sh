5048d96877805cea4cb497f1aecaed543bdfddac
Joe Corall <jjc223@lehigh.edu>
2026-03-04 17:46:02 -0500

    Persist certs after rake

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -7,13 +7,7 @@ pushd /opt/sandbox/isle-site-template
 
 cp /opt/sandbox/.env .
 
-sed -i '/^      --ping=true$/i\
-      --entrypoints.https.http.tls.certResolver=letsencrypt\
-      --certificatesresolvers.letsencrypt.acme.httpchallenge=true\
-      --certificatesresolvers.letsencrypt.acme.httpchallenge.entrypoint=http\
-      --certificatesresolvers.letsencrypt.acme.storage=/acme/acme.json\
-      --certificatesresolvers.letsencrypt.acme.email=community@islandora.ca\
-      --certificatesresolvers.letsencrypt.acme.caserver=https://acme-v02.api.letsencrypt.org/directory' docker-compose.yml
+cp /opt/sandbox/docker-compose.override.yml .
 
 mkdir -p ./secrets
 cp /opt/sandbox/.secrets/ACTIVEMQ_WEB_ADMIN_PASSWORD ./secrets/ACTIVEMQ_WEB_ADMIN_PASSWORD
