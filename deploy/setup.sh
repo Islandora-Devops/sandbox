6ebbf7764d3f7382663d0cdec5c2a9817db55332
Joe Corall <jjc223@lehigh.edu>
2026-03-04 14:40:14 -0500

    Enable acme cert resolver

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -6,6 +6,15 @@ git clone https://github.com/islandora-devops/isle-site-template /opt/sandbox/is
 pushd /opt/sandbox/isle-site-template
 
 cp /opt/sandbox/.env .
+
+sed -i '/^      --ping=true$/i\
+      --entrypoints.https.http.tls.certResolver=letsencrypt\
+      --certificatesresolvers.letsencrypt.acme.httpchallenge=true\
+      --certificatesresolvers.letsencrypt.acme.httpchallenge.entrypoint=http\
+      --certificatesresolvers.letsencrypt.acme.storage=/acme/acme.json\
+      --certificatesresolvers.letsencrypt.acme.email=community@islandora.ca\
+      --certificatesresolvers.letsencrypt.acme.caserver=https://acme-v02.api.letsencrypt.org/directory' docker-compose.yml
+
 mkdir -p ./secrets
 cp /opt/sandbox/.secrets/ACTIVEMQ_WEB_ADMIN_PASSWORD ./secrets/ACTIVEMQ_WEB_ADMIN_PASSWORD
 cp /opt/sandbox/.secrets/DRUPAL_DEFAULT_ACCOUNT_PASSWORD ./secrets/DRUPAL_DEFAULT_ACCOUNT_PASSWORD
